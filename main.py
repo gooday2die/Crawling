d73cf97873305216c5975e8ce4e9400d217ec783
gooday2die <49092508+gooday2die@users.noreply.github.com>
2019-03-30 21:29:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 
 main_url = "http://books.toscrape.com/index.html"
 result = requests.get(main_url)
-
 result.text[:1000]
 soup = BeautifulSoup(result.text, 'html.parser')
 
@@ -15,7 +14,6 @@ soup = BeautifulSoup(result.text, 'html.parser')
 def getBookInfo():
     main_page_products_urls = [x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")]
     print(str(len(main_page_products_urls)) + " fetched products URLs")
-    print("One example:")
 
     i = 0
     while (i < 20):
@@ -24,5 +22,26 @@ def getBookInfo():
 
 
 
+#getBookInfo()
 
-getBookInfo()
+
+
+
+def getBookName():
+    main_page_products_urls = [x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")]
+
+    i = 0
+    print(len(main_page_products_urls))
+    while (i < len(main_page_products_urls)):
+        request_url = "http://books.toscrape.com/"+str(main_page_products_urls[i])
+        #print(request_url)
+        result2 = requests.get(request_url)
+        result2.text[:1000]
+        soup2 = BeautifulSoup(result2.text, 'html.parser')
+        title = soup2.find("h1")
+        #print(soup2)
+        BookName = title.get_text()
+        print("# " + str(i+1) +" " +str(BookName))
+        i = i + 1
+
+getBookName()
