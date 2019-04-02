7c9dbbebbf33ca60d21746d831618a2238d7554f
gooday2die <49092508+gooday2die@users.noreply.github.com>
2019-04-02 20:17:24 +0900

    Update main.py
    Used 1001Tracklist API for searching tracklists.
    When we query using their API they return us JSON file as return.
    Searched some basic JSON knowledge.
    First I was trying to separate the result manually, but I found that it was using JSON.
    
    Now we can search new tracklists from a certain artist.
    And we can also see the tracks played in the tracklist.
    
    GetTracks : for getting tracks from tracklist
    GetTracklist : for getting tracklists from certain search
    gettracklistcommand() : getting command (just two functions combined together for cleanness)
    
    
    Future plans for tracklists.
    - Save tracklist from artists in a DB
    - Find similarity between sets
    - Use Beatport API and make a playlist.
    - Use Spotify API and make a playlist. (https://github.com/plamere/spotipy seems to be python module for Spotify)
    - Expect what to come next using AI (Which seems to be tough challenge)

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import requests
-from bs4 import BeautifulSoup
 import re
-
+import json
 
 main_url = "http://books.toscrape.com/index.html"
 result = requests.get(main_url)
@@ -12,6 +10,7 @@ soup = BeautifulSoup(result.text, 'html.parser')
 
 #print(soup.prettify()[:1000])
 
+#For Book Crawling Functions Start
 
 
 no = 1
@@ -135,13 +134,14 @@ def getSpecificBookImg(no):
 
 
 
+
 #Function Starts from Here.
 #Right Below is Global Declaration
 #getCommand Function is a recursive function.
 
 main_page_products_urls = [x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")]
 totalBookNO = len(main_page_products_urls)
-print("There are " + str(len(main_page_products_urls)) +" Books in the page")
+#print("There are " + str(len(main_page_products_urls)) +" Books in the page")
 
 command = 0
 
@@ -177,7 +177,7 @@ def getCommand():
             getSpecificBookImg(i)
             print("")
             i = i + 1
-            
+
         getCommand()
 
     if (command == "Q" or command == "q"):
@@ -188,5 +188,88 @@ def getCommand():
         print("Wrong Value")
         print("")
         getCommand()
+#For Book Crawling Functions Ended
 
-getCommand()
+#getCommand()
+
+
+
+#For Tracklist Crawling Functions Start
+
+def GetTracks(URL):
+    print("")
+    main_url="https://1001tracklist-api.azurewebsites.net/api/tracklist/tracks?tracklistURL="+str(URL)
+    result = requests.get(main_url)
+    JSONlist = json.loads(str(result.text)) #for JSON Query
+    #print(json.dumps(JSONlist, indent=2, sort_keys=True)) #For JSON's list
+
+    maxlength =  (len(JSONlist["tracks"])) #maxlength is for lines of JSON files
+
+    i = 0
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("")
+    print("There are " + str(maxlength) + " tracks in this set")
+    print("")
+
+
+
+    while(i< maxlength):
+        print("")
+        print("# " + str(i + 1) + " Track")
+        print(JSONlist['tracks'][i]['trackTitle'])
+        print("")
+
+        i = i + 1
+
+    #newresult = json.loads(str(result))
+
+    #Tracklists = (((((result.text.replace("},{", "\n")).replace("\"trackTitle\":","").replace("{\"tracks\":[{",""))).replace(",\"timestamp\":*","").replace("}]}","")))
+    #print(Tracklists)
+
+
+def GetTracklist(search):
+    print("")
+    main_url="https://1001tracklist-api.azurewebsites.net/api/tracklists?query=" + str(search)
+    result = requests.get(main_url)
+
+    JSONlist = json.loads(str(result.text)) #for JSON Query
+    #print(JSONlist) #for Printout
+    #print(json.dumps(JSONlist, indent=2, sort_keys=True)) #For JSON's list
+
+    #JSON
+
+    maxlength =  (len(JSONlist["results"])) #maxlength is for lines of JSON files
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("")
+    print("We found " + str(maxlength)+ " recent tracklists")
+    print("")
+
+    i = 0
+    while ( i < maxlength ):
+        print("")
+        print (JSONlist['results'][i]['link'])
+        print (JSONlist['results'][i]['title'])
+        print ("")
+
+        i = i + 1
+
+    #link = JSONlist['results'][0]['link']
+    #title = JSONlist['results'][0]['title']
+
+    #print(title)
+    #print(link)
+
+    #SearchLists = (((result.text.replace("},{","\n")).replace("{\"results\":[{","")).replace("}]}","")).replace("\"title\":","")
+    #print(SearchLists)
+
+def gettracklistcommand():
+    print("Query for :")
+    search = input("")
+    GetTracklist(search)
+
+    print("")
+    print("Tracklist For :")
+    URL = input("")
+    GetTracks(URL)
+
+gettracklistcommand()
